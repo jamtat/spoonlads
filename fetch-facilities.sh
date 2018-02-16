5b8c64df651fac1ab7bf7451801f40895697224c
Ben Tattersley <ubershinysheep@gmail.com>
2018-02-16 21:55:33 +0000

    Update facilities data format

--- fetch-facilities.sh
+++ fetch-facilities.sh
@@ -2,4 +2,4 @@
 
 curl -s "https://www.jdwetherspoon.com/pubs/all-pubs" | \
 	pup ':parent-of(.js-facilities) json{}' | \
- 	jq '. | map(.children | add | {id: .value, label: .text})'
+ 	jq '. | map(.children | add | {(.value): .text}) | add'
